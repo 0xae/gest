b1ed02050f427abccc6e45b19b8771d250e62f76
Ayrton Gomes <com.ayrton@gmail.com>
2018-09-07 09:01:09 -0100

    working on stuff

---
+++ src/Admin/Backend/Form/OrderType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Admin\Backend\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class OrderType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('quantity')
+            ->add('obs')
+            ->add('status')
+            ->add('createdAt')
+            ->add('product')
+            ->add('createdBy')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Admin\Backend\Entity\Order'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'admin_backend_order';
+    }
+}
