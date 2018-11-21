e77479f449453c0c175b4225e61f9dc942549237
Ayrton Gomes <com.ayrton@gmail.com>
2018-11-21 15:02:48 -0100

    fix calculation for complaint book

--- src/Admin/Backend/Controller/NotificationController.php
+++ src/Admin/Backend/Controller/NotificationController.php
@@ -24,9 +24,11 @@ class NotificationController extends Controller {
         $em = $this->getDoctrine()->getManager();
         $type = @$_GET['type']?$_GET['type'] : 'complaint';
         $oType='type';
+        $days=15;
 
         if ($type == 'comp_book') {
             $oType="'comp_book'";
+            $days=10;
         } else if ($type=='reclamation_internal') {
             $oType="'reclamation_internal'";
         }
@@ -37,7 +39,7 @@ class NotificationController extends Controller {
             created_at,
             date_add(created_at, interval 15 day) as resp_date,
             state, 
-            datediff(date_add(created_at, interval 15 day), date_format(now(), '%Y-%m-%d')) as days_left,
+            datediff(date_add(created_at, interval $days day), date_format(now(), '%Y-%m-%d')) as days_left,
             (select codigo from app_entity where id = (select entity from user where id=T.created_by)) as department,
             date_format(created_at, '%Y') as year
         from $type T
